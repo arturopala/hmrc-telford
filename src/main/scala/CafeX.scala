4d39580bc0774c60ecf0309fb8a200e4e7200b12
Artur Opala <artur.opala@siili.fi>
2017-01-20 18:44:10 +0100

    implementation fulfilling step 1 requirements

--- src/main/scala/CafeX.scala
+++ src/main/scala/CafeX.scala
@@ -6,7 +6,12 @@ case class Item(name: String, price: BigDecimal)
 
 class CafeX(menu: List[Item]) {
 
-    def bill(items: List[String]): Try[BigDecimal] = ???
+    def findItem(item: String): Item =
+        menu.find(_.name == item).getOrElse(throw new Exception(s"Menu item '$item' not found"))
+
+    def bill(items: List[String]): Try[BigDecimal] = Try {
+        items.map(findItem).map(_.price).sum
+    }
 
 }
 
