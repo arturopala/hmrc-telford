41b078861913400dec4aa92c6ae200af35435268
Artur Opala <artur.opala@siili.fi>
2017-01-20 19:23:12 +0100

    implementation of step 2 requirements

--- src/main/scala/CafeX.scala
+++ src/main/scala/CafeX.scala
@@ -2,15 +2,25 @@ package hmrc.exercise
 
 import scala.util.Try
 
-case class Item(name: String, price: BigDecimal)
+case class Item(name: String, isFood: Boolean, isHot: Boolean, price: BigDecimal)
 
 class CafeX(menu: List[Item]) {
 
-    def findItem(item: String): Item =
+    def bill(items: List[String]): Try[BigDecimal] = Try {
+        val purchase = items.map(findItem)
+        val total = purchase.map(_.price).sum
+        total + tip(purchase, total)
+    }
+
+    private def findItem(item: String): Item =
         menu.find(_.name == item).getOrElse(throw new Exception(s"Menu item '$item' not found"))
 
-    def bill(items: List[String]): Try[BigDecimal] = Try {
-        items.map(findItem).map(_.price).sum
+    private def tip(items: List[Item], total: BigDecimal) = {
+        val hasHotFood = items.exists(i => i.isHot && i.isFood)
+        val hasFood = items.exists(_.isFood)
+        if (hasHotFood) (total * BigDecimal("0.20")).min(BigDecimal("20.00"))
+        else if (hasFood) total * BigDecimal("0.10")
+        else BigDecimal("0.00")
     }
 
 }
